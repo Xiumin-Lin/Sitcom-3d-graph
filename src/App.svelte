fd3f1dc4a5a970737437ce1e0ff71b181412127e
Xiumin-Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2021-08-02 18:06:02 +0200

    Add 3d-force-graph

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,29 @@
 <script>
 	import { trad as t } from './store';
+	import ForceGraph3D from '3d-force-graph';
+	import { onMount } from 'svelte';
 	export let name;
+	let graphDiv = undefined;
+
+	// Random tree
+	const N = 300;
+	const gData = {
+		nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+		links: [...Array(N).keys()]
+			.filter((id) => id)
+			.map((id) => ({
+				source: id,
+				target: Math.round(Math.random() * (id - 1)),
+			})),
+	};
+	onMount(() => {
+		const Graph = ForceGraph3D()(graphDiv).graphData(gData);
+	});
 </script>
 
 <main>
 	<h1>{$t['hello']} {name}!</h1>
+	<div id="3d-graph" bind:this={graphDiv} />
 </main>
 
 <style>
